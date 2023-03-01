f4f094e58825e2bf1e96036f6ac4b7e84c7347be
jonathanritchelly <69868418+jonathanritchelly@users.noreply.github.com>
2023-03-01 09:20:21 -0300

    Update filial.controller.js

--- api_ligacao/controllers/filial.controller.js
+++ api_ligacao/controllers/filial.controller.js
@@ -91,7 +91,7 @@ exports.novoContatoSite = async (req, res) => {
   var mensagem = req.body.mensagem;
 
   console.log(nome);
-  res.status(200).json(req.body);
+  //res.status(200).json(req.body);
 
   var remetente = nodemailer.createTransport({
     host: "smtp.hostinger.com.br",
