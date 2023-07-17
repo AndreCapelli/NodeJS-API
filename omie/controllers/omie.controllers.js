904ad6bdc8d810afebd912ab09d0e2d5334314b9
jonathanritchelly <69868418+jonathanritchelly@users.noreply.github.com>
2023-07-17 15:09:04 -0300

    Update omie.controllers.js

--- omie/controllers/omie.controllers.js
+++ omie/controllers/omie.controllers.js
@@ -32,26 +32,28 @@ exports.OmiePedido = async (req, res) => {
 };
 
 exports.OmieProduto = async (req, res) => {
-  async function insereContato() {
-    await sequelize
-      .query(
-        `INSERT INTO integracao_Omie (inJson, inSetor, inData, inDescricaoEtapa, inCodigo) 
+  if (JSON.stringify(req.body) != '{"ping":"omie"}') {
+    async function insereContato() {
+      await sequelize
+        .query(
+          `INSERT INTO integracao_Omie (inJson, inSetor, inData, inDescricaoEtapa, inCodigo) 
         Values ('${JSON.stringify(req.body)}','Produto', GetDate(),' ${
-          req.body.event.etapa + " - " + req.body.event.etapaDescr
-        }',' ${req.body.event.idPedido}')`,
-        {
-          type: QueryTypes.INSERT,
-        }
-      )
-      .catch((err) => {
-        res.status(500).json({
-          message: err.message + " Omie!",
+            /*req.body.event.etapa +*/ " - " /*+ req.body.event.etapaDescr*/
+          }',' */${req.body.event.idPedido}/*5050')`,
+          {
+            type: QueryTypes.INSERT,
+          }
+        )
+        .catch((err) => {
+          res.status(500).json({
+            message: err.message + " Omie!",
+          });
         });
-      });
-    return;
-  }
+      return;
+    }
 
-  await insereContato();
+    await insereContato();
+  }
 
   res.status(200).send("ok");
 };
