f194c62a8c92a2e47f865f7db30efdb2561dfd3c
jonathanritchelly <69868418+jonathanritchelly@users.noreply.github.com>
2023-07-17 11:18:16 -0300

    Update server.js

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ const options = {
   cert: fs.readFileSync("./SSL/code.crt"),
 };
 
-https.createServer(options, apps).listen(4443, () => {
-  console.log("Servidor HTTPS iniciado na porta 443");
+https.createServer(options, apps).listen(21443, () => {
+  console.log("Servidor HTTPS iniciado na porta 21443");
 });
 
 apps.get("/", (req, res) => {
