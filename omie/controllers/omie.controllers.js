8b2ecfdbc9bede028c5785a530e1187b42cf86e4
jonathanritchelly <69868418+jonathanritchelly@users.noreply.github.com>
2023-07-17 14:56:21 -0300

    Update omie.controllers.js

--- omie/controllers/omie.controllers.js
+++ omie/controllers/omie.controllers.js
@@ -10,7 +10,7 @@ exports.OmiePedido = async (req, res) => {
       .query(
         `INSERT INTO integracao_Omie (inJson, inSetor, inData, inDescricaoEtapa, inCodigo) 
         Values ('${JSON.stringify(req.body)}','Pedido', GetDate(),' ${
-          req.body.event.etapa + " " + req.body.event.etapaDescr
+          req.body.event.etapa + " - " + req.body.event.etapaDescr
         }',' ${req.body.event.idPedido}')`,
         {
           type: QueryTypes.INSERT,
@@ -30,7 +30,27 @@ exports.OmiePedido = async (req, res) => {
 };
 
 exports.OmieProduto = async (req, res) => {
-  console.log("Produto: " + req.body);
+  async function insereContato() {
+    await sequelize
+      .query(
+        `INSERT INTO integracao_Omie (inJson, inSetor, inData, inDescricaoEtapa, inCodigo) 
+        Values ('${JSON.stringify(req.body)}','Produto', GetDate(),' ${
+          req.body.event.etapa + " - " + req.body.event.etapaDescr
+        }',' ${req.body.event.idPedido}')`,
+        {
+          type: QueryTypes.INSERT,
+        }
+      )
+      .catch((err) => {
+        res.status(500).json({
+          message: err.message + " Omie!",
+        });
+      });
+    return;
+  }
+
+  await insereContato();
+
   res.status(200).send("ok");
 };
 
