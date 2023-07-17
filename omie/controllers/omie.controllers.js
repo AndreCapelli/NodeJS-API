5ce51e69319a8b57670a377542e79bc30dfc9bb7
jonathanritchelly <69868418+jonathanritchelly@users.noreply.github.com>
2023-07-17 16:14:35 -0300

    Update omie.controllers.js

--- omie/controllers/omie.controllers.js
+++ omie/controllers/omie.controllers.js
@@ -6,19 +6,26 @@ exports.OmiePedido = async (req, res) => {
   console.log("Pedido: " + JSON.stringify(req.body));
 
   if (JSON.stringify(req.body) != '{"ping":"omie"}') {
-    await sequelize
-      .query(
-        `DELETE FROM WHERE inSetor = 'Pedido' AND inCodigo =
-      '${req.body.event.idPedido}'`,
-        {
-          type: QueryTypes.DELETE,
-        }
-      )
-      .catch((err) => {
-        res.status(500).json({
-          message: err.message + " Omie!",
+    var codigo = req.body.event.idPedido.toString();
+    codigo = codigo.replace(" ", "");
+
+    async function apagar() {
+      await sequelize
+        .query(
+          `DELETE FROM WHERE inSetor = 'Pedido' AND Replace(inCodigo,' ','') =
+      '${codigo}'`,
+          {
+            type: QueryTypes.DELETE,
+          }
+        )
+        .catch((err) => {
+          res.status(500).json({
+            message: err.message + " Omie!",
+          });
         });
-      });
+    }
+
+    await apagar();
 
     async function insereContato() {
       await sequelize
@@ -26,7 +33,7 @@ exports.OmiePedido = async (req, res) => {
           `INSERT INTO integracao_Omie (inJson, inSetor, inData, inDescricaoEtapa, inCodigo) 
           Values ('${JSON.stringify(req.body)}','Pedido', GetDate(),' ${
             req.body.event.etapa + " - " + req.body.event.etapaDescr
-          }',' ${req.body.event.idPedido}')`,
+          }',' ${codigo}')`,
           {
             type: QueryTypes.INSERT,
           }
@@ -48,7 +55,6 @@ exports.OmiePedido = async (req, res) => {
 exports.OmieProduto = async (req, res) => {
   if (JSON.stringify(req.body) != '{"ping":"omie"}') {
     var codigo = req.body.event.codigo_produto.toString();
-
     codigo = codigo.replace(" ", "");
 
     async function apagar() {
@@ -97,27 +103,34 @@ exports.OmieProduto = async (req, res) => {
 
 exports.OmieCliente = async (req, res) => {
   if (JSON.stringify(req.body) != '{"ping":"omie"}') {
-    await sequelize
-      .query(
-        `DELETE FROM WHERE inSetor = 'Cliente' AND inCodigo =
-    '${req.body.event.codigo_cliente_omie}'`,
-        {
-          type: QueryTypes.DELETE,
-        }
-      )
-      .catch((err) => {
-        res.status(500).json({
-          message: err.message + " Omie!",
+    var codigo = req.body.event.codigo_cliente_omie.toString();
+    codigo = codigo.replace(" ", "");
+
+    async function apagar() {
+      await sequelize
+        .query(
+          `DELETE FROM WHERE inSetor = 'Cliente' AND Replace(inCodigo,' ','') =
+    '${codigo}'`,
+          {
+            type: QueryTypes.DELETE,
+          }
+        )
+        .catch((err) => {
+          res.status(500).json({
+            message: err.message + " Omie!",
+          });
         });
-      });
+    }
+
+    await apagar();
 
     async function insereContato() {
       await sequelize
         .query(
           `INSERT INTO integracao_Omie (inJson, inSetor, inData, inCodigo) 
-        Values ('${JSON.stringify(req.body)}','Cliente', GetDate(),' ${
-            req.body.event.codigo_cliente_omie
-          }')`,
+        Values ('${JSON.stringify(
+          req.body
+        )}','Cliente', GetDate(),' ${codigo}')`,
           {
             type: QueryTypes.INSERT,
           }
