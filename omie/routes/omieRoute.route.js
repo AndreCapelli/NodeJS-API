d293c9974e5bda3dfbd7dd032b2efc5ddc9b1acc
jonathanritchelly <69868418+jonathanritchelly@users.noreply.github.com>
2023-07-17 11:37:19 -0300

    Update omieRoute.route.js

--- omie/routes/omieRoute.route.js
+++ omie/routes/omieRoute.route.js
@@ -1,7 +1,7 @@
 module.exports = (app) => {
   const omie = require("../controllers/omie.controllers.js");
   const router = require("express").Router();
-  router.get("/omie_Pedido/", omie.OmiePedido);
+  router.post("/omie_Pedido/", omie.OmiePedido);
 
   app.use("/omie", router);
 };
